57b3d8ecefaac0130f6f91128ba26846b3657662
Yshmael Ammonraheem <yautomate@gmail.com>
2017-05-12 09:10:38 -0500

    Initial Package
    The 1st attempt to commit

---
+++ V1.0.0.0/Development/EasyClickTravelTest/EasyClickTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
+using System.IO;
+using System.Reflection;
+
+namespace EasyClickTravelTest
+{
+    [TestClass]
+    public class EasyClickTest
+    {
+        [TestMethod]
+        public void OpenFireFoxTest()
+        {       
+            var outputPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase);
+            string solutionConfigurationType = Path.GetFileName(outputPath);
+            string assemblyLocation = null;            
+            if (File.Exists(@"..\\..\\..\\..\\Execution\\" + solutionConfigurationType + "\\geckodriver.exe"))
+            {
+                assemblyLocation = "Debug";
+            }
+                else
+            {
+                assemblyLocation = "Release";
+            }
+            string driverPath = "..\\..\\..\\..\\Execution\\" + assemblyLocation + "\\";
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(driverPath);
+            FirefoxDriver driver = new FirefoxDriver(service);
+            driver.Manage().Window.Maximize();
+            driver.Url = "http://www.easyclicktravel.com";
+            Thread.Sleep(5000);
+            driver.Dispose();   
+        }
+    }
+}
---
+++ V1.0.0.1/Development/EasyClickTravelTest/blah.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
+using System.IO;
+using System.Reflection;
